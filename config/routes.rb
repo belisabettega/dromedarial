3412777831c151a193d7989f8d6861924f447daa
belisa bettega <belisabettega@gmail.com>
2021-05-31 18:34:05 -0300

    added basic routes to the models

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  resources :users, except: [:destroy, :edit, :update]
+  resources :categories, only: [:new, :create] do
+    resources :videos, only: [:new, :create]
+  end
+  resources :videos, only: [:edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
