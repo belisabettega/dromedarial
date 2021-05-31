f6668604d002af328caf376989074b58431eb57d
Pedro Barbosa <pedrovbb89@gmail.com>
2021-05-31 19:14:36 -0300

    Added user seed to seed file + added faker to the gem file enviorment

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts 'Deleting all users'
+User.destroy_all
+puts 'Users deleted'
+
+puts 'Deleting all categories'
+Category.destroy_all
+puts 'Categories destroyed'
+
+puts 'Creating new users DB'
+
+10.times do
+  user = User.new(
+    email: Faker::Internet.unique.email,
+    encrypted_password: 'test',
+    overview: Faker::Lorem.paragraph,
+    name: Faker::Name.unique.name,
+  )
+end
+
+puts 'Users created'
