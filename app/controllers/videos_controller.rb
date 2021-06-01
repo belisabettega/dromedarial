b55e16bb2d371afc6a09ab2adcab5f1e20e4be8b
belisa bettega <belisabettega@gmail.com>
2021-06-01 20:10:30 -0300

    links to add or refuse video and artist dashboard grid/flexbox

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,2 +1,30 @@
 class VideosController < ApplicationController
+    before_action :set_video
+    def edit
+    end
+   
+    def update
+      @video.status = "completed"
+      if @video.update(videos_params)
+        redirect_to dashboard_path
+      else
+        render 'edit'
+      end
+    end
+
+    def change_status
+      @video.status = "denied"
+      @video.save
+      redirect_to dashboard_path
+    end
+   
+    private
+
+    def set_video
+      @video = Video.find(params[:id])
+    end
+   
+    def videos_params
+      params.require(:video).permit(:content)
+    end
 end
