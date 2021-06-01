3ab5d59863bba89452f745cba725048a3dcc5e53
David Pessanha <davidppessanha@gmail.com>
2021-06-01 17:37:29 -0300

    video request method created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,15 @@
 class UsersController < ApplicationController
-  before_action :set_users, only:[:show]
+  before_action :set_users, only:[:show, :video_request]
 
   def index
     @users = User.where(role: 'artist')
   end
 
   def show
+  end
+
+  def video_request
+    @video = Video.new
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,7 @@ Rails.application.routes.draw do
       get :video_request
     end
   end
-  resources :categories, only: [:new, :create] do
-    resources :videos, only: [:new, :create]
-  end
-  resources :videos, only: [:edit, :update]
+  resources :categories, only: [:new, :create]
+  resources :videos, only: [:edit, :update, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
