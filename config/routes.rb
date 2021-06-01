26dbc3babe22021d97c788da5d49c87b6a75efc1
belisa bettega <belisabettega@gmail.com>
2021-06-01 17:37:38 -0300

    dashboard artist - finished new and create related to categories

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
     resources :videos, only: [:new, :create]
   end
   resources :videos, only: [:edit, :update]
+  get 'dashboard', to: 'users#dashboard'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
