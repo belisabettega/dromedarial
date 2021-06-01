f5ef859449d5b08d4844f6503fe59036a36b51bb
David Pessanha <davidppessanha@gmail.com>
2021-06-01 20:50:02 -0300

    video_request method and page not done yet

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,5 +1,19 @@
 class VideosController < ApplicationController
-  def video_request
-    @video = Video.new
+
+  def create
+    @video = Video.new(video_params)
+    @video.user_id = current_user
+
+    if @video.save && @video.content.nil?
+      redirect_to root_path
+    else
+      render 'users/video_request'
+    end
+  end
+
+  private
+
+  def video_params
+    params.require(:video).permit(:category_id)
   end
 end
