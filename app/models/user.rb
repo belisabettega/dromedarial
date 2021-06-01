331eae6bd23ea65e06ce4e520b6923cbcd67b162
Maykon Franchini <maykonfranchini2017@gmail.com>
2021-06-01 16:40:01 +0100

    create role array inside User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :categories
   has_many :videos
+  ROLE = ['artist', 'costumer']
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
 end
