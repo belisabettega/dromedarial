028b5f33f90840b5eadbc29a1e056b03544736b2
belisa bettega <belisabettega@gmail.com>
2021-06-01 12:33:22 -0300

    droped database and create and migrate new one with the new model role variable

--- db/schema.rb
+++ db/schema.rb
@@ -34,7 +34,7 @@ ActiveRecord::Schema.define(version: 2021_05_31_211910) do
     t.datetime "updated_at", precision: 6, null: false
     t.text "overview"
     t.string "name"
-    t.string "type"
+    t.string "role"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
