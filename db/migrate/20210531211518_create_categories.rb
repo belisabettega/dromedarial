7ad68a7780cfbdea00d39ae4a589371ea230aa5e
Pedro Barbosa <pedrovbb89@gmail.com>
2021-06-01 13:18:34 -0300

    Changed the categories migration from type:string to title:string

--- db/migrate/20210531211518_create_categories.rb
+++ db/migrate/20210531211518_create_categories.rb
@@ -1,7 +1,7 @@
 class CreateCategories < ActiveRecord::Migration[6.0]
   def change
     create_table :categories do |t|
-      t.string :type
+      t.string :title
       t.float :price
       t.references :user, null: false, foreign_key: true
 
