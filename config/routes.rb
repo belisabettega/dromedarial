9ecf04e35942346d69f12c216d26033f64f72289
belisa bettega <belisabettega@gmail.com>
2021-06-02 12:24:18 -0300

    fix conflits in the router

--- config/routes.rb
+++ config/routes.rb
@@ -4,16 +4,12 @@ Rails.application.routes.draw do
   resources :users, except: [:index, :destroy, :edit, :update] do
     resources :videos, only: [:new]
   end
-<<<<<<< HEAD
   resources :categories, only: [:new, :create]
   resources :videos, only: [:edit, :update, :create]
-=======
-  resources :videos, only: [:edit, :update]
   get 'dashboard', to: 'users#dashboard'
   get 'purchases', to: 'users#purchases'
   get 'change_status/:id', to: 'videos#change_status', as: :change_status
   get 'review/:id', to: 'videos#review', as: :add_review
   patch 'set_review/:id', to: 'videos#set_review', as: :set_review
->>>>>>> master
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
